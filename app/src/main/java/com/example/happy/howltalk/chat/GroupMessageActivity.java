ef8143ad9313966f0189a841dab3a71416d20387
JungHoon Park <wfr112@naver.com>
2018-05-10 19:39:27 +0900

    update2

--- app/src/main/java/com/example/happy/howltalk/chat/GroupMessageActivity.java
+++ app/src/main/java/com/example/happy/howltalk/chat/GroupMessageActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,11 +16,14 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.example.happy.howltalk.R;
 import com.example.happy.howltalk.model.ChatModel;
 import com.example.happy.howltalk.model.UserModel;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -26,10 +31,13 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TimeZone;
 
 public class GroupMessageActivity extends AppCompatActivity {
 
@@ -39,8 +47,9 @@ public class GroupMessageActivity extends AppCompatActivity {
     private EditText editText;
     private DatabaseReference databaseReference;
     private ValueEventListener valueEventListener;
-    private List<ChatModel.Comment> comments = new ArrayList<>();
     private RecyclerView recyclerView;
+    private SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy.MM.dd HH:mm");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,10 +57,8 @@ public class GroupMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_group_message);
         destinationRoom = getIntent().getStringExtra("destinationRoom");
         editText = findViewById(R.id.groupMessageActivity_editText);
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
 
-        recyclerView = findViewById(R.id.groupMessageActivity_recyclerView);
-        recyclerView.setAdapter(new GroupMessageRecyclerViewAdapter());
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         FirebaseDatabase.getInstance().getReference().child("users").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
@@ -59,7 +66,11 @@ public class GroupMessageActivity extends AppCompatActivity {
                 for (DataSnapshot item: dataSnapshot.getChildren()) {
                     users.put(item.getKey(), item.getValue(UserModel.class));
                 }
+
                 init();
+                recyclerView = findViewById(R.id.groupMessageActivity_recyclerView);
+                recyclerView.setAdapter(new GroupMessageRecyclerViewAdapter());
+                recyclerView.setLayoutManager(new LinearLayoutManager(GroupMessageActivity.this));
             }
 
             @Override
@@ -91,8 +102,10 @@ public class GroupMessageActivity extends AppCompatActivity {
     }
 
     class GroupMessageRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+        private List<ChatModel.Comment> comments = new ArrayList<>();
 
         public GroupMessageRecyclerViewAdapter() {
+            //값 불러옴
             getMessageList();
         }
 
@@ -102,7 +115,7 @@ public class GroupMessageActivity extends AppCompatActivity {
                 public void onDataChange(DataSnapshot dataSnapshot) {
                     comments.clear();
 
-                    for (DataSnapshot item : dataSnapshot.getChildren()) {
+                    for (DataSnapshot item: dataSnapshot.getChildren()) {
                         comments.add(item.getValue(ChatModel.Comment.class)); //리스트에 아이템 추가함.
                     }
                     notifyDataSetChanged();
@@ -127,7 +140,34 @@ public class GroupMessageActivity extends AppCompatActivity {
 
         @Override
         public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+            GroupMessageViewHolder messageViewHolder = (GroupMessageViewHolder)holder;
+            //홀더안에있는 뷰에 data를 넣어줌
+            //내가쓸 말풍선
+            if (comments.get(position).uid.equals(uid)) {
+                messageViewHolder.textView_message.setText(comments.get(position).message);
+                messageViewHolder.textView_message.setBackgroundResource(R.drawable.rightbubble);
+                //채팅시 내 사진은 안보여줘도 되므로
+                messageViewHolder.linearLayout_destination.setVisibility(View.INVISIBLE);
+                messageViewHolder.linearLayout_main.setGravity(Gravity.RIGHT);
+            } else { //상대방
+                Glide.with(holder.itemView.getContext())
+                        .load(users.get(comments.get(position).uid).profileImageUrl)
+                        .apply(new RequestOptions().circleCrop())
+                        .into(messageViewHolder.imageView_profile);
+                messageViewHolder.textView_name.setText(users.get(comments.get(position).uid).userName);
+                messageViewHolder.linearLayout_destination.setVisibility(View.VISIBLE);
+                messageViewHolder.textView_message.setBackgroundResource(R.drawable.leftbubble);
+                messageViewHolder.textView_message.setText(comments.get(position).message);
+                messageViewHolder.textView_message.setTextSize(25f);
+                messageViewHolder.linearLayout_main.setGravity(Gravity.LEFT);
+            }
 
+            //시간처리
+            long unixTime = (long) comments.get(position).timeStamp;
+            Date date = new Date(unixTime);
+            simpleDateFormat.setTimeZone(TimeZone.getTimeZone("Asia/Seoul"));
+            String time = simpleDateFormat.format(date);
+            messageViewHolder.textView_timeStamp.setText(time);
         }
 
         @Override
