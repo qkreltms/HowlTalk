1e59c2e11ca4666fb649d511e058805bbd63b079
JungHoon Park <wfr112@naver.com>
2018-05-10 18:05:00 +0900

    update

--- app/src/main/java/com/example/happy/howltalk/chat/GroupMessageActivity.java
+++ app/src/main/java/com/example/happy/howltalk/chat/GroupMessageActivity.java
@@ -3,9 +3,16 @@ package com.example.happy.howltalk.chat;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.happy.howltalk.R;
 import com.example.happy.howltalk.model.ChatModel;
@@ -14,20 +21,26 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ServerValue;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GroupMessageActivity extends AppCompatActivity {
 
-    Map<String, UserModel> users = new HashMap<>();
-    String destinationRoom;
-    String uid;
-    EditText editText;
-
+    private Map<String, UserModel> users = new HashMap<>();
+    private String destinationRoom;
+    private String uid;
+    private EditText editText;
+    private DatabaseReference databaseReference;
+    private ValueEventListener valueEventListener;
+    private List<ChatModel.Comment> comments = new ArrayList<>();
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +49,16 @@ public class GroupMessageActivity extends AppCompatActivity {
         destinationRoom = getIntent().getStringExtra("destinationRoom");
         editText = findViewById(R.id.groupMessageActivity_editText);
 
+        recyclerView = findViewById(R.id.groupMessageActivity_recyclerView);
+        recyclerView.setAdapter(new GroupMessageRecyclerViewAdapter());
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
         FirebaseDatabase.getInstance().getReference().child("users").addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                users = (Map<String, UserModel>) dataSnapshot.getValue();
+                for (DataSnapshot item: dataSnapshot.getChildren()) {
+                    users.put(item.getKey(), item.getValue(UserModel.class));
+                }
                 init();
             }
 
@@ -69,5 +88,71 @@ public class GroupMessageActivity extends AppCompatActivity {
                 });
             }
         });
+    }
+
+    class GroupMessageRecyclerViewAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+        public GroupMessageRecyclerViewAdapter() {
+            getMessageList();
+        }
+
+        public void getMessageList() {
+            FirebaseDatabase.getInstance().getReference().child("chatrooms").child(destinationRoom).child("comments").addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    comments.clear();
+
+                    for (DataSnapshot item : dataSnapshot.getChildren()) {
+                        comments.add(item.getValue(ChatModel.Comment.class)); //리스트에 아이템 추가함.
+                    }
+                    notifyDataSetChanged();
+
+                    recyclerView.scrollToPosition(comments.size() - 1);
+                }
+
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
+
+                }
+            });
+        }
+
+        @NonNull
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_message, parent, false);
+
+            return new GroupMessageViewHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return comments.size();
+        }
+
+        private class GroupMessageViewHolder extends RecyclerView.ViewHolder {
+            public TextView textView_message;
+            public TextView textView_name;
+            public ImageView imageView_profile;
+            public LinearLayout linearLayout_destination;
+            public LinearLayout linearLayout_main;
+            public TextView textView_timeStamp;
+
+
+            public GroupMessageViewHolder(View view) {
+                super(view);
+                textView_message = view.findViewById(R.id.messageItem_textView_message);
+                textView_name = view.findViewById(R.id.messageItem_textView_name);
+                imageView_profile = view.findViewById(R.id.messageItem_imageView_profile);
+                linearLayout_destination = view.findViewById(R.id.messageItem_linearLayout_destination);
+                linearLayout_main = view.findViewById(R.id.messageItem_linearLayout_main);
+                textView_timeStamp = view.findViewById(R.id.messageItem_textView_timeStamp);
+            }
+        }
     }
 }
